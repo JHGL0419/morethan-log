69f14dfc77afbe40e2efb632e9f30fd7d93268c9
JeongHyun Han <81086444+JHGL0419@users.noreply.github.com>
2023-05-29 19:28:42 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,12 +1,11 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "JeongHyun Han",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "Computer Science Student",
     bio: "Hanyang Univ - ICEWALL",
     email: "supercsehan@gmail.com",
-    linkedin: "morethanmin",
     github: "JHGL0419",
     instagram: "jeong_h.go",
   },
@@ -18,14 +17,14 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "CSE STUDY",
+    description: "welcome to CSE STUDY!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://morethan-JHGL0419.vercel.app",
+  since: 2023, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
   seo: {
