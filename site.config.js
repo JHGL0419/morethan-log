e7f31a90261d269477b7c22d04e216f6cafbd6e6
JeongHyun Han <81086444+JHGL0419@users.noreply.github.com>
2023-05-29 19:32:42 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -6,6 +6,7 @@ const CONFIG = {
     role: "Computer Science Student",
     bio: "Hanyang Univ - ICEWALL",
     email: "supercsehan@gmail.com",
+    linkedin: false,
     github: "JHGL0419",
     instagram: "jeong_h.go",
   },
